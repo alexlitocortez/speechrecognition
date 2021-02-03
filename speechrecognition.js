873c5ebbc77b4c9851e60663190b0e4eebd51ad5
Alex Cortez <acim650@gmail.com>
2021-02-03 10:50:11 -0800

    speechrecognition.js

---
+++ speechrecognition.js
@@ -0,0 +1,32 @@
+
+window.SpeechRecognition = window.SpeechRecognition || window,
+    webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('results', e => {
+    console.log(e.results);
+    const transcript = Array.from(e.results);
+    .map(results => result[0])
+    .map(result => result.transcript);
+    .join('');
+
+    p.textContent = transcript;
+    if(e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+    if(transcript.includes('unicorn')) {
+        console.log('unicorn');
+    }
+    console.log(transcript);
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
